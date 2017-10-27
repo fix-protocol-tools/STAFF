3a5dc6491b3cc7ecbd4453ecbfe2b2db6b3fa782
Alexander Sereda <Alexander_Sereda@epam.com>
2017-10-27 16:18:53 +0300

    fixed STAFF running script

--- staff-distribution/src/main/bin/staff.bat
+++ staff-distribution/src/main/bin/staff.bat
@@ -13,4 +13,4 @@ set STAFF_HOME_DIR=%~dp0..
 
 set CLASSPATH="%STAFF_HOME_DIR%\lib\ant-launcher-${ant.version}.jar"
 set JAVA_OPTS="-Dstaff.log.dir=%STAFF_HOME_DIR%\logs" "-Dant.home=%STAFF_HOME_DIR%" "-Djava.library.path=%STAFF_HOME_DIR%\lib"
-%JAVA_BIN% -classpath %CLASSPATH% %JAVA_OPTS% org.apache.tools.ant.launch.Launcher %* -cp "%STAFF_HOME_DIR%\lib\staff-ant-annotations.jar;%STAFF_HOME_DIR%\lib\staff-core.jar;%STAFF_HOME_DIR%\config"
+%JAVA_BIN% -classpath %CLASSPATH% %JAVA_OPTS% org.apache.tools.ant.launch.Launcher %* -cp "%STAFF_HOME_DIR%\lib\staff-ant-annotations-${project.version}.jar;%STAFF_HOME_DIR%\lib\staff-core-${project.version}.jar;%STAFF_HOME_DIR%\config"
